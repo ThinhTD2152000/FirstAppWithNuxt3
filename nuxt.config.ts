0fc9ce1592b61df20777f9365bdf6be717a444ad
YenWoody <yenph@fimo.edu.vn>
2023-08-09 22:28:39 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/css/main.css',
-  ],
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxtjs/tailwindcss'],
   build: {
     transpile: ['@headlessui/vue'],
   },
-  
 })
